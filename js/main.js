acaaed01260697b431661ced4d0f4db6de3718ee
Reshmi R S <44020004+reshmisaji@users.noreply.github.com>
2019-01-05 07:52:33 +0530

    extracted some util functions

--- js/main.js
+++ js/main.js
@@ -1,98 +1,152 @@
 let counter = 1;
-const dayNight = ['nightMode', 'dayMode'];
+const dayNight = ["nightMode", "dayMode"];
 const modes = {
-	'nightMode': nightMode = function () {
-		changeBackgroundColor('workspace', 'black');
-		changeBackgroundColor('inputPart', 'rgb(44, 42, 42)');
-		changeBackgroundColor('page-header', 'black');
-		changeFontColor('inputPart', 'white');
-		changeFontColor('page-header', 'white');
-		document.getElementById('nightMode').src = 'images/daymode.png';
-	},
-	'dayMode': dayMode = function () {
-		changeBackgroundColor('workspace', 'white');
-		changeBackgroundColor('inputPart', ' rgb(236, 234, 234)');
-		changeBackgroundColor('page-header', 'white');
-		changeFontColor('page-header', 'black');
-		changeFontColor('inputPart', 'black');
-		document.getElementById('nightMode').src = 'images/nightmode.png';
-	}
-}
-const images = ['images/bg.jpg', 'images/background.jpg', 'images/blueandred.jpg'];
-const fontFamilies = ["Arial", "monospace", "Times New Roman", "Times", "serif", "Georgia", "Palatino Linotype",
-	"Book Antiqua", "Palatino",
-	"Helvetica", "sans-serif", "Arial Black", "Gadget", "Comic Sans MS", "cursive", "Impact", "Charcoal",
-	"Lucida Sans Unicode",
-	"Lucida Grande", "Tahoma", "Geneva", "Trebuchet MS", "Verdana", "Courier New", "Courier", "Lucida Console",
-	"Monaco"
+  nightMode: (nightMode = function() {
+    changeBackgroundColor("workspace", "black");
+    changeBackgroundColor("inputPart", "rgb(44, 42, 42)");
+    changeBackgroundColor("page-header", "black");
+    changeFontColor("inputPart", "white");
+    changeFontColor("page-header", "white");
+    getElement("nightMode").src = "images/daymode.png";
+  }),
+  dayMode: (dayMode = function() {
+    changeBackgroundColor("workspace", "white");
+    changeBackgroundColor("inputPart", " rgb(236, 234, 234)");
+    changeBackgroundColor("page-header", "white");
+    changeFontColor("page-header", "black");
+    changeFontColor("inputPart", "black");
+    getElement("nightMode").src = "images/nightmode.png";
+  })
+};
+const images = [
+  "images/bg.jpg",
+  "images/background.jpg",
+  "images/blueandred.jpg"
+];
+const fontFamilies = [
+  "Arial",
+  "monospace",
+  "Times New Roman",
+  "Times",
+  "serif",
+  "Georgia",
+  "Palatino Linotype",
+  "Book Antiqua",
+  "Palatino",
+  "Helvetica",
+  "sans-serif",
+  "Arial Black",
+  "Gadget",
+  "Comic Sans MS",
+  "cursive",
+  "Impact",
+  "Charcoal",
+  "Lucida Sans Unicode",
+  "Lucida Grande",
+  "Tahoma",
+  "Geneva",
+  "Trebuchet MS",
+  "Verdana",
+  "Courier New",
+  "Courier",
+  "Lucida Console",
+  "Monaco"
 ];
 
-const getValue = function () {
-	let bgImage = "url('" + document.getElementById('image').value + "')";
-	let backgroundColour = document.getElementById('bgcolor').value;
-	let fontType = document.getElementById('font').value;
-	let fontColor = document.getElementById('color').value;
-	let letterSize = document.getElementById('size').value + 'px';
-	let data = document.getElementById('text').value;
-
-	document.getElementById('outputPart').style.backgroundImage = bgImage;
-	changeBackgroundColor('output', backgroundColour);
-	document.getElementById('output').style.fontFamily = fontType;
-	changeFontColor('output', fontColor);
-	document.getElementById('output').style.fontSize = letterSize;
-	putHtmlCode('output', data);
+const getElement = function(id) {
+  return document.getElementById(id);
 };
 
-const changeBackgroundColor = function (block, anyColor) {
-	document.getElementById(block).style.backgroundColor = anyColor;
-}
+const getElementValue = function(elementId) {
+  return getElement(elementId).value;
+};
 
-const changeFontColor = function (block, anyColor) {
-	document.getElementById(block).style.color = anyColor;
-}
+const getImageAddress = function(elementId) {
+  return "url('" + getElementValue(elementId) + "')";
+};
 
-const putHtmlCode = function (block, code) {
-	document.getElementById(block).innerHTML = code;
-}
+const setBackgroundImage = function(blockId, elementId) {
+  return (getElement(blockId).style.backgroundImage = getImageAddress(
+    elementId
+  ));
+};
 
-const putHtmlData = function (block, data) {
-	document.getElementById(block).innerHTML = data.join("");
-}
+const setFontFamily = function(blockId, elementId) {
+  return (getElement(blockId).style.fontFamily = getElementValue(elementId));
+};
 
-const refresh = function () {
-	location.reload();
-}
+const setFontSize = function(blockId, elementId) {
+  return (getElement(blockId).style.fontSize =
+    getElementValue(elementId) + "px");
+};
 
-const getOption = function (value) {
-	return "<option value='" + value + "'>" + value + "</option>";
-}
+const getValue = function() {
+  setBackgroundImage("outputPart", "image");
+  changeBackgroundColor("output", getElementValue("bgcolor"));
+  setFontFamily("output", "font");
+  setFontSize("output", "size");
+  changeFontColor("output", getElementValue("color"));
+  putHtmlCode("output", getElementValue("text"));
+};
 
-const toggler = function () {
-	counter = counter + 1;
-	modes[dayNight[counter % 2]]();
-}
+const changeBackgroundColor = function(block, anyColor) {
+  getElement(block).style.backgroundColor = anyColor;
+};
 
-const getTitle = function (headElement) {
-	let title = headElement.split("</title>")[0];
-	title = title.split("<title>")[1];
-	return title;
-}
+const changeFontColor = function(block, givenColor) {
+  getElement(block).style.color = givenColor;
+};
 
-const getCode = function () {
-	let markup = document.getElementById("code").value;
-	let headElement = markup.split("<body>");
-	let body = headElement[1];
-	headElement = headElement[0];
-	let title = getTitle(headElement);
+const putHtmlCode = function(block, code) {
+  getElement(block).innerHTML = code;
+};
 
-	putHtmlCode('tabHead', title);
-	putHtmlCode('codeOutput', body);
-}
+const putHtmlData = function(block, data) {
+  getElement(block).innerHTML = data.join("");
+};
 
-const loadData = function () {
-	let options = fontFamilies.map(getOption);
-	document.getElementById('font').innerHTML = options.join("");
+const refresh = function() {
+  location.reload();
+};
 
-	let optionImages = images.map(getOption);
-	document.getElementById('image').innerHTML = optionImages.join("");
-}
+const getOption = function(value) {
+  return "<option value='" + value + "'>" + value + "</option>";
+};
+
+const toggler = function() {
+  counter = counter + 1;
+  modes[dayNight[counter % 2]]();
+};
+
+const getTitle = function(headElement) {
+  let title = headElement.split("</title>")[0];
+  title = title.split("<title>")[1];
+  return title;
+};
+
+const getCode = function() {
+  let markup = getElementValue("code");
+  let headElement = markup.split("<body>");
+  let body = headElement[1];
+  headElement = headElement[0];
+  let title = getTitle(headElement);
+
+  putHtmlCode("tabHead", title);
+  putHtmlCode("codeOutput", body);
+};
+
+// const highlight = function() {
+//   let val = getElementValue("code");
+//   let length = val.length - 2;
+//   let lastCharacter = val.slice(length);
+//   if (lastCharacter == "<") {
+//     getElementValue("code").slice(length).style.color = "red";
+//   }
+// };
+
+const loadData = function() {
+  let optionImages = images.map(getOption);
+  let options = fontFamilies.map(getOption);
+  putHtmlData("font", options);
+  putHtmlData("image", optionImages);
+};

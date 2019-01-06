b71f69a803100be689b4a827613aad05e238da79
Reshmi R S <44020004+reshmisaji@users.noreply.github.com>
2019-01-06 10:20:58 +0530

    add some features for the quiz part

--- images/Page Title_files/main.js
+++ images/Page Title_files/main.js
@@ -1,6 +1,5 @@
 let counter = 1;
 let questionNumber = 0;
-let score = 0;
 const dayNight = ["nightMode", "dayMode"];
 const modes = {
   nightMode: (nightMode = function() {
@@ -60,34 +59,10 @@ const quiz = [
   {
     question: "What does HTML stand for ?",
     options: [
-      "1.Home Tool Markup Language\n",
-      "2.Hyper Text Markup Language\n",
-      "3.Hyperlinks and Text Markup Language\n"
+      "Home Tool Markup Language",
+      "Hyper Text Markup Language",
+      "Hyperlinks and Text Markup Language"
     ],
-    answer: "2"
-  },
-  {
-    question: "Choose the correct HTML element for the largest heading:",
-    options: ["1.<heading>\n", "2.<h6>\n", "3.<h1>\n"],
-    answer: "3"
-  },
-  {
-    question: "What is the correct HTML element for inserting a line break?",
-    options: ["1.<br>\n", "2.<break>\n", "3.<lb>\n"],
-    answer: "1"
-  },
-  {
-    question: "What is the correct HTML for adding a background color?",
-    options: [
-      '1.<body style="background-color:yellow;">\n',
-      "2.<background>yellow</background>\n",
-      '3.<body bg="yellow">\n'
-    ],
-    answer: "1"
-  },
-  {
-    question: "Choose the correct HTML element to define important text",
-    options: ["1.<important>\n", "2.<strong>\n", "3.<b>\n"],
     answer: "2"
   }
 ];
@@ -179,40 +154,21 @@ const rightAnswer = function() {
   changeFontColor("message", "white");
   getElement("message").style.textAlign = "center";
   putHtmlCode("message", "WOW ! \n You Are Right");
-  score += 10;
 };
 
 const wrongAnswer = function() {
   changeBackgroundColor("codeOutput", "red");
   changeFontColor("message", "white");
   getElement("message").style.textAlign = "center";
-  putHtmlCode("message", "OOPS ! You Are Wrong");
-};
-
-const gameOver = function() {
-  changeFontColor("message", "white");
-  changeBackgroundColor("codeOutput", "black");
-  getElement("message").style.textAlign = "center";
-  putHtmlCode("message", "GAME OVER !");
-  changeBackgroundColor("question", "gray");
-  getElement("question").style.textAlign = "center";
-  getElement("question").style.paddingTop = "155px";
-  getElement("question").style.borderRadius = "5%";
-  getElement("answer").innerHTML = "";
-  if (quiz.length * 10 == score) {
-    putHtmlCode("question", "<h1>You got all the correct answers</h1>");
-  } else {
-    putHtmlCode("question", "<h1>YOUR SCORE : " + score + "</h1>");
-  }
+  putHtmlCode("message", "OOPS ! \n You Are Wrong");
 };
 
 const displayNext = function() {
   getElement("next").innerHTML =
-    "<button id='nextButton' type='button' onclick='loadQuestion()'>NEXT>></button>";
+    "<button id='nextButton' type='button' >NEXT>></button>";
   changeBackgroundColor("nextButton", "black");
   changeFontColor("nextButton", "white");
   getElement("nextButton").style.fontSize = "20px";
-  getElement("qAnswer").value = "";
 };
 
 const checkAnswer = function() {
@@ -225,7 +181,6 @@ const checkAnswer = function() {
   }
   displayNext();
   questionNumber++;
-  putHtmlCode("tabHead", "SCORE : " + score);
 };
 
 const loadData = function() {
@@ -236,12 +191,9 @@ const loadData = function() {
 };
 
 const loadQuestion = function() {
-  if (questionNumber == quiz.length) {
-    gameOver();
-  }
   let data = quiz[questionNumber];
   getElement("qn").innerText = data.question;
-  getElement("option").innerText = data.options.map(x => x).join("");
-  getElement("message").innerHTML = "";
-  changeBackgroundColor("codeOutput", "rgb(236, 234, 234");
+  getElement("option").innerHTML = data.options
+    .map(x => "<li>" + x + "</li>")
+    .join("");
 };
